d182bee844cde5ebc47417c1721975fdc2ff3b1c
mabc224 <mabc224@gmail.com>
2022-10-20 20:55:17 +0500

    Add orders module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module, Logger } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'nestjs-prisma';
 import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule.forRoot({ isGlobal: true }),
     ProductsModule,
+    OrdersModule,
   ],
   controllers: [],
   providers: [Logger],
